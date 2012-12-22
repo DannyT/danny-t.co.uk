deedf55ced486518f0fb8c89955a38aaedd0719c
DannyT <dan@moov2.com>
2012-12-22 03:26:44 +0000

    finally got intuit included

---
+++ css/scss/watch.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Change all instances of ‘dannyt’ to whatever you have named your
+# project’s stylesheet, `cd` to the directory in which this file lives and
+# simply run `sh watch.sh`.
+
+# No minification
+sass --watch dannyt.scss:../dannyt.css --style expanded
+
+#sass --watch dannyt.scss:../dannyt.min.css --style compressed
+
+exit 0
