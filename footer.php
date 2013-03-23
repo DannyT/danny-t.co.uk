9d89bf86f5b1eb75632657c287213d83846ad413
DannyT <dan@moov2.com>
2013-03-23 10:52:03 +0000

    fixing inuit submodule 1

--- footer.php
+++ footer.php
@@ -32,7 +32,7 @@
     <script defer>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
   <![endif]-->
  <script>
-    var _gaq=[['_setAccount','UA-648363-2'],['_trackPageview']];
+    var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
     (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
     g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
     s.parentNode.insertBefore(g,s)}(document,'script'));
